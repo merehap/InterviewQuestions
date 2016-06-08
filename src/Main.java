76bbbc266be27bcf771a2f915b8a6b6a54aa2f45
sean <merehap@gmail.com>
2016-06-08 14:16:55 -0700

    Added iterative TreeTraversal.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,36 @@
 
-public class Main {
 
+
+public class Main {
+	public static void main(String[] args) {
+		TreeTraversals.Tree<Integer> tree = new TreeTraversals.Tree<Integer>(
+				new TreeTraversals.Tree<>(
+						new TreeTraversals.Tree<>(
+								null,
+								3,
+								new TreeTraversals.Tree<>(
+										null,
+										4,
+										null)),
+						2,
+						new TreeTraversals.Tree<>(
+								null,
+								5,
+								null)),
+				1,
+				new TreeTraversals.Tree<>(
+						new TreeTraversals.Tree<>(
+								null,
+								7,
+								null),
+						6,
+						new TreeTraversals.Tree<>(
+								null,
+								8,
+								null)));
+		
+		for(int value : TreeTraversals.iterative_preorder(tree)) {
+			System.out.println(value);
+		}
+	}
 }
---
+++ src/TreeTraversals.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+public class TreeTraversals {
+	
+	public static <T> List<T> preorder(Tree<T> tree) {
+		List<T> result = new ArrayList<>();
+		if(tree == null) {
+			return result;
+		}
+		
+		result.add(tree.value);
+		result.addAll(preorder(tree.left));
+		result.addAll(preorder(tree.right));
+		
+		return result;
+	}
+	
+	public static <T> List<T> iterative_preorder(Tree<T> tree) {
+		
+		if(tree == null) {
+			return new ArrayList<>();
+		}
+		
+		List<T> result = new ArrayList<>();
+		Stack<Tree<T>> stack = new Stack<>();
+		
+		Tree<T> node = tree;
+		while(node != null) {
+			
+			if(stack.empty() || node != stack.peek()) {
+				while(node != null) {
+					result.add(node.value);
+					stack.push(node);
+					node = node.left;
+				}
+				
+				node = stack.pop();
+			}
+			
+			while(node.right == null) {
+				if(stack.empty()) {
+					break;
+				}
+				node = stack.pop();
+			}
+			
+			node = node.right;
+		}
+		
+		return result;
+	}
+	
+	
+	public static class Tree<T> {
+		private final Tree<T> left;
+		private final T value;
+		private final Tree<T> right;
+		
+		public Tree(final Tree<T> left, final T value, final Tree<T> right) {
+			this.left = left;
+			this.value = value;
+			this.right = right;
+		}
+		
+		public Tree<T> getLeft() {
+			return this.left;
+		}
+		
+		public T getValue() {
+			return this.value;
+		}
+		
+		public Tree<T> getRight() {
+			return this.right;
+		}
+	}
+}
